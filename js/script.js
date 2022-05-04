30d3b7bf0d1bf6a668e3886cb6448de2bda213c5
Silvia-Du <silviadu@hotmail.it>
2022-05-04 09:28:06 +0200

    Corretto button per far comparire una nuova immagine e modificarla, (provvisorio)

--- js/script.js
+++ js/script.js
@@ -6,13 +6,38 @@ const app = new Vue({
     message: 'Questa è la prima cosa che scrivo in Vue',
     sfondo: 'img-background',
     image: './generic_vue_header.jpg',
+    buttonText: 'Fai comparire un\'altra immagine',
+
     visibilità: 'hide',
-    visible: false
+    roundedBorder: 'rounded',
+    isRounded:false,
+    visible: true,
+    counter: 0,
   },
 
   methods:{
     toggleGetImage(){
-      this.visible = ! this.visible;
+
+      this.counter ++;
+      console.log(this.counter);
+
+      if( this.counter == 1 ){
+        this.visible = !this.visible;
+        this.buttonText = 'ora modificala!';
+      }
+      
+
+      if( this.counter == 2 ){
+        this.isRounded = !this.isRounded;
+        this.visible = false;
+        this.buttonText = 'falla sparire!'
+      }
+
+      if( this.counter == 3 ){
+        this.buttonText= 'Fallo di nuovo!';
+        this.visible = true;
+        this.counter = 1;
+      }
     }
   }
 
